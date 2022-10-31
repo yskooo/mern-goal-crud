7ba70f3a67bb2c58b21687f1d4b41a5cb3ed2285
Harold Martin <yskomartin@gmail.com>
2022-10-31 17:21:46 +0800

    Create userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,81 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+// @desc Register new user
+// @route POST api/user
+// @access Public
+const registerUser = asyncHandler(async(req, res) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+
+    //Check if user exists
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create User
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
+
+// @desc Authenticate user
+// @route POST api/user/login
+// @access Public
+const loginUser = asyncHandler(async(req, res) => {
+    const { email, password } = req.body
+
+    // Check for user email
+    const user = await User.findOne({ email })
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
+})
+
+// @desc Get user data
+// @route GET api/users/me
+// @access Public
+const getMe = asyncHandler(async(req, res) => {
+    res.json({ message: 'User data display' })
+})
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe,
+}
