c6ceca15bebd8e7098e531577a375244b89582e8
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2022-11-04 15:05:18 +0800

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,36 @@
+import {useState, useEffect} from 'react'
+import {FaUser} from 'react-icons'
 
 function Register() {
-  return (
-    <div>
-      Register
-    </div>
-  )
-}
+    const [formData, setFormData] = useState({
+      name: '',
+      email:'',
+      password:'',
+      password2:'',
+    })
+const {name, email, password, password2} = formData
+  
+const onChange = () => {}
+return <>
+  <section className="heading">
+    <h1>
+      <FaUser/> Register
+    </h1>
+    <p>Please create an account</p>
+  </section>
 
+  <section className="form">
+    <form action="">
+      <input type="text" 
+        className='form-control'
+        id='name'
+        name='name'
+        value={name}
+        placeholder='Enter your name'
+        onChange={onChange}
+      />
+    </form>
+  </section>
+</>
+}
 export default Register
