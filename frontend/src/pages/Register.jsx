b87f8fb5581d7ced1b38fcacb7ef3c7e2c6f9bb5
Harold Martin <yskomartin@gmail.com>
2022-11-05 11:15:54 +0800

    fix: polyfill error

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import {FaUser} from 'react-icons'
+import {FaUser} from 'react-icons/fa'
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -9,8 +9,18 @@ function Register() {
       password2:'',
     })
 const {name, email, password, password2} = formData
-  
-const onChange = () => {}
+
+const onChange = (e) => {
+  setFormData((prevState) => ({
+    ...prevState,
+    [e.target.name]: e.target.value,
+  }))
+}
+
+const onSubmit = (e) => {
+  e.preventDefault()
+}
+
 return <>
   <section className="heading">
     <h1>
@@ -20,7 +30,8 @@ return <>
   </section>
 
   <section className="form">
-    <form action="">
+    <form onSubmit={onSubmit}>
+      <div className="form-group">
       <input type="text" 
         className='form-control'
         id='name'
@@ -29,6 +40,40 @@ return <>
         placeholder='Enter your name'
         onChange={onChange}
       />
+      </div>
+      <div className="form-group">
+      <input type="email" 
+        className='form-control'
+        id='email'
+        name='email'
+        value={email}
+        placeholder='Enter your email'
+        onChange={onChange}
+      />
+      </div>
+      <div className="form-group">
+      <input type="text" 
+        className='form-control'
+        id='password'
+        name='password'
+        value={password}
+        placeholder='Enter password'
+        onChange={onChange}
+      />
+      </div>
+      <div className="form-group">
+      <input type="text" 
+        className='form-control'
+        id='password2'
+        name='password2'
+        value={password2}
+        placeholder='Confirm password'
+        onChange={onChange}
+      />
+      </div>
+      <div className="form-group">
+        <button type="submit" className="btn btn-block">Submit</button>
+      </div>
     </form>
   </section>
 </>
